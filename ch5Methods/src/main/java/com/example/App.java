27690855a6400482aa812c138336fa1ed7e8dfd3
Ahmad Haroun <Ahmad.haroun2023@gmail.com>
2024-10-03 08:55:33 +0300

    added ch5

---
+++ ch5Methods/src/main/java/com/example/App.java
@@ -0,0 +1,30 @@
+package com.example;
+
+import java.text.ListFormat.Style;
+
+import javax.sound.midi.SysexMessage;
+import java.util.Arrays;
+import jakarta.faces.event.SystemEvent;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        System.out.println("======================================================");
+        System.out.println("======================================================");
+        System.out.println("======================================================");
+
+        
+
+
+
+
+
+
+
+        System.out.println("======================================================");
+        System.out.println("======================================================");
+        System.out.println("======================================================");
+    }
+}
